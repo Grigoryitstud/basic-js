11b29941eae48d59643d8e2ac50f70ca06a6ee92
Grigiryitstud <initstudentitin@gmail.com>
2022-05-01 23:40:35 +0300

    refactor: only one task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,4 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
@@ -14,11 +13,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  
 }
-
 module.exports = {
   countCats
 };
