db4d92b284ed41e17bdea757f5d74a48c210be08
Grigiryitstud <initstudentitin@gmail.com>
2022-05-03 00:56:20 +0300

    refactor: thats all(

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function calculateHanoi(disks, turnsSpeed) {
+  let turnsNumber = Math.pow(2, disks) - 1;
+  let secondsNumber = Math.floor(turnsNumber / (turnsSpeed/3600));
+  let calculate = {
+    turns: turnsNumber,
+    seconds: secondsNumber
+  }
+return calculate;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(Array.isArray(arr) == false){
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  if(arr.length == 0) {
+    return [];
+  } 
+  for(let i = 0; i < arr.length; i++) {
+    if(typeof(arr[i]) == 'string') {
+      if(arr[i] == '--discard-next'){
+        if(arr[i + 1] !== undefined) {
+          arr.splice(i, 2);
+        } else {
+          arr.splice(i, 1);
+        }
+      }
+      if(arr[i] == '--discard-prev'){
+        if(arr[i - 1] !== undefined) {
+          arr.splice(i-1, 2);
+        } else {
+          arr.splice(i, 1);
+        }
+      }
+      if(arr[i] == '--double-next'){
+        if(arr[i + 1] !== undefined) {
+          arr.splice(i, 1, arr[i + 1]);
+        } else {
+          arr.splice(i, 1);
+        }
+      }
+      if(arr[i] == '--double-prev'){
+        if(arr[i - 1] !== undefined) {
+          arr.splice(i, 1, arr[i - 1]);
+        } else {
+          arr.splice(i, 1);
+          }      
+      }
+    return arr
+    }
+  }
+  // let func = arr.map(a => a == '--discard-next' ? )
 }
 
 module.exports = {
